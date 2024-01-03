0dcc28e648cc835f175982f9100c6e3f2d2258f1
Guilherme Ferreira <ferreiraga@outlook.com>
2024-01-03 17:57:27 -0300

    Added Helath endpoint

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { databaseProviders } from './database.providers';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, HealthModule],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
   exports: [...databaseProviders]
---
+++ api/src/health/health.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('health')
+export class HealthController {
+
+    @Get()
+    getHello(): string {
+        return "Healthy";
+    }
+}
