cb3bc3d69d79fc92b1a80af683d563f2db1082fa
Guilherme Ferreira <ferreiraga@outlook.com>
2024-01-03 12:05:30 -0300

    Way simpler app

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as express from 'express'
+const app = express()
+const port = 3000
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
